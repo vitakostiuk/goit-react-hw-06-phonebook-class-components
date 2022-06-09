b46e4bd272460c9740250d9d7616f85cd35b5d49
Vita Kostiuk <vitagrebennik@gmail.com>
2022-06-09 16:07:21 +0300

    initial commit

---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import shortid from 'shortid';
+import s from './ContactForm.module.css';
+
+export class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  static propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.currentTarget.name]: e.currentTarget.value,
+    });
+  };
+
+  handleSubmit = e => {
+    const { name, number } = this.state;
+    e.preventDefault();
+
+    this.props.onSubmit({ name, number });
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    const nameId = shortid.generate();
+    const numberId = shortid.generate();
+    const { name, number } = this.state;
+
+    return (
+      <form className={s.ContactForm} onSubmit={this.handleSubmit}>
+        <div className={s.InputWrapper}>
+          <label className={s.Label} htmlFor={nameId}>
+            Name
+          </label>
+          <input
+            id={nameId}
+            className={s.Input}
+            type="text"
+            name="name"
+            value={name}
+            onChange={this.handleChange}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </div>
+
+        <div className={s.InputWrapper}>
+          <label className={s.Label} htmlFor={numberId}>
+            Number
+          </label>
+          <input
+            id={numberId}
+            className={s.Input}
+            type="tel"
+            name="number"
+            value={number}
+            onChange={this.handleChange}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </div>
+        <button type="submit" className={s.FormBtn}>
+          Add contact
+        </button>
+      </form>
+    );
+  }
+}
